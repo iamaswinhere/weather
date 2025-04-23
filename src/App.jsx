1c47ab78ff087660bee124b0f30f5e552998cf17
iamaswinhere <aswin005achu@gmail.com>
2025-04-23 20:27:13 +0530

    Project Started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello from satellite</h1>
+      <img src="/public/Images/weatherLogo.svg" alt="" style={{width:100}}/>
+    </>
+  )
+}
+
+export default App
